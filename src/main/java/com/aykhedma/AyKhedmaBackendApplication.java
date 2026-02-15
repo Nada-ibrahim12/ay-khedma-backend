f44dded79bf0427b7363c1953000ab70a0b669dd
Nada-ibrahim12 <123187920+Nada-ibrahim12@users.noreply.github.com>
2026-02-16 01:10:09 +0200

    structure

---
+++ src/main/java/com/aykhedma/AyKhedmaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aykhedma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyKhedmaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AyKhedmaBackendApplication.class, args);
+    }
+
+}
