0f751fb1c403d83266ccd40310a6f2e52f5ba083
Nada-ibrahim12 <123187920+Nada-ibrahim12@users.noreply.github.com>
2026-02-16 03:32:37 +0200

    fix security

--- src/main/java/com/aykhedma/AyKhedmaBackendApplication.java
+++ src/main/java/com/aykhedma/AyKhedmaBackendApplication.java
@@ -2,8 +2,10 @@ package com.aykhedma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("file:.env")
 public class AyKhedmaBackendApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/aykhedma/TestAyKhedmaBackendApplication.java
+++ src/test/java/com/aykhedma/TestAyKhedmaBackendApplication.java
@@ -1,11 +1,11 @@
-package com.aykhedma;
-
-import org.springframework.boot.SpringApplication;
-
-public class TestAyKhedmaBackendApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.from(AyKhedmaBackendApplication::main).with(TestcontainersConfiguration.class).run(args);
-    }
-
-}
+//package com.aykhedma;
+//
+//import org.springframework.boot.SpringApplication;
+//
+//public class TestAyKhedmaBackendApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.from(AyKhedmaBackendApplication::main).with(TestcontainersConfiguration.class).run(args);
+//    }
+//
+//}
